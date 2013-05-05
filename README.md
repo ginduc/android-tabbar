6d027048fa6f76919ee2db3672a42a5513ab3514
Giancarlo Inductivo <gian@dynamicobjx.com>
2013-05-05 10:13:14 +0800

    initial..

---
+++ README.md
@@ -0,0 +1,11 @@
+Dev Notes
+=========
+
+1. Install the supplied Android Support v4 Jar on your local maven repo:
+`mvn install:install-file -Dfile=libs/android-support-v4.jar -DgroupId=com.google.android -DartifactId=support-v4 -Dversion=r12 -Dpackaging=jar`
+
+2. Run maven command to build the entire project:
+`mvn clean package`
+
+3.
+
---
+++ android-tabbar-it/src/main/java/com/dynobjx/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.dynobjx.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.dynobjx.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
