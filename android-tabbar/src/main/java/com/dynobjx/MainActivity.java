fe84a96df2fc0ccbfce8e460423b4d180ca7cf15
Giancarlo Inductivo <gian@dynamicobjx.com>
2013-05-06 15:16:17 +0800

    moved tabhost to the bottom, realtabcontent now displaying correctly..

--- android-tabbar/src/main/java/com/dynobjx/MainActivity.java
+++ android-tabbar/src/main/java/com/dynobjx/MainActivity.java
@@ -14,31 +14,31 @@ import com.dynobjx.fragments.SettingsFragment;
 import com.dynobjx.fragments.TopRatedFragment;
 
 public class MainActivity extends SherlockFragmentActivity {
-	@InjectView(android.R.id.tabhost) FragmentTabHost tabHost;
+	@InjectView(android.R.id.tabhost) FragmentTabHost ftabHost;
 	 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Views.inject(this);
-        tabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        ftabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
         
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(R.string.app_name);
         actionBar.setDisplayShowHomeEnabled(false);
         
-        tabHost.addTab(tabHost.newTabSpec("Top").setIndicator(null, getResources().getDrawable(R.drawable.btn_toprated)),
+        ftabHost.addTab(ftabHost.newTabSpec("Top").setIndicator(null, getResources().getDrawable(R.drawable.btn_toprated)),
                 TopRatedFragment.class, null);
-        tabHost.addTab(tabHost.newTabSpec("Search").setIndicator(null, getResources().getDrawable(R.drawable.btn_search)),
+        ftabHost.addTab(ftabHost.newTabSpec("Search").setIndicator(null, getResources().getDrawable(R.drawable.btn_search)),
                 SearchFragment.class, null);
-        tabHost.addTab(tabHost.newTabSpec("Home").setIndicator(null, getResources().getDrawable(R.drawable.btn_home)),
+        ftabHost.addTab(ftabHost.newTabSpec("Home").setIndicator(null, getResources().getDrawable(R.drawable.btn_home)),
                 HomeFragment.class, null);
-        tabHost.addTab(tabHost.newTabSpec("Settings").setIndicator(null, getResources().getDrawable(R.drawable.btn_settings)),
+        ftabHost.addTab(ftabHost.newTabSpec("Settings").setIndicator(null, getResources().getDrawable(R.drawable.btn_settings)),
                 SettingsFragment.class, null);
-        tabHost.addTab(tabHost.newTabSpec("Faves").setIndicator(null, getResources().getDrawable(R.drawable.btn_favorites)),
+        ftabHost.addTab(ftabHost.newTabSpec("Faves").setIndicator(null, getResources().getDrawable(R.drawable.btn_favorites)),
                 FavoritesFragment.class, null);
 
-        tabHost.setCurrentTab(2);
+        ftabHost.setCurrentTabByTag("Home");
     }
 
 }
