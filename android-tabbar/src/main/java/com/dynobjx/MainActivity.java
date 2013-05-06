48f76b2365ee08dba558825473cff85e13e96d44
Giancarlo Inductivo <gian@dynamicobjx.com>
2013-05-06 13:46:07 +0800

    added new logo, refactored main activity..

--- android-tabbar/src/main/java/com/dynobjx/MainActivity.java
+++ android-tabbar/src/main/java/com/dynobjx/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dynobjx;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 import butterknife.InjectView;
 import butterknife.Views;
 
@@ -10,86 +14,74 @@ import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.dynobjx.fragments.HomeFragment;
 import com.dynobjx.fragments.TopRatedFragment;
 
-public class MainActivity extends SherlockFragmentActivity {
-	@InjectView(android.R.id.tabhost) TabHost tHost;
+public class MainActivity extends SherlockFragmentActivity implements OnTabChangeListener {
+	@InjectView(android.R.id.tabhost) TabHost tabHost;
 	 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Views.inject(this);
-        tHost.setup();
+        tabHost.setup();
         
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("OnTheMoney");
+        actionBar.setTitle(R.string.app_name);
         actionBar.setDisplayShowHomeEnabled(false);
 
-        TabHost.OnTabChangeListener tabChangeListener = new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String tabId) {
-                android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-                HomeFragment androidFragment = (HomeFragment) fm.findFragmentByTag("Home");
-                TopRatedFragment appleFragment = (TopRatedFragment) fm.findFragmentByTag("Top");
-                android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
+        tabHost.setOnTabChangedListener(this);
  
-                /** Detaches the androidfragment if exists */
-                if(androidFragment!=null)
-                    ft.detach(androidFragment);
- 
-                /** Detaches the applefragment if exists */
-                if(appleFragment!=null)
-                    ft.detach(appleFragment);
- 
-                /** If current tab is android */
-                if(tabId.equalsIgnoreCase("android")){
- 
-                    if(androidFragment==null){
-                        /** Create AndroidFragment and adding to fragmenttransaction */
-                        ft.add(R.id.realtabcontent,new HomeFragment(), "android");
-                    }else{
-                        /** Bring to the front, if already exists in the fragmenttransaction */
-                        ft.attach(androidFragment);
-                    }
- 
-                }else{    /** If current tab is apple */
-                    if(appleFragment==null){
-                        /** Create AppleFragment and adding to fragmenttransaction */
-                        ft.add(R.id.realtabcontent,new TopRatedFragment(), "apple");
-                     }else{
-                        /** Bring to the front, if already exists in the fragmenttransaction */
-                        ft.attach(appleFragment);
-                    }
-                }
-                ft.commit();
+        addTab("Top", getResources().getDrawable(R.drawable.btn_toprated));
+        addTab("Search", getResources().getDrawable(R.drawable.btn_search));
+        addTab("Home", getResources().getDrawable(R.drawable.btn_home));
+        addTab("Settings", getResources().getDrawable(R.drawable.btn_settings));
+        addTab("Faves", getResources().getDrawable(R.drawable.btn_favorites));
+        tabHost.setCurrentTab(2);
+    }
+    
+    @Override
+    public void onTabChanged(String tabId) {
+        FragmentManager fm = getSupportFragmentManager();
+        HomeFragment home = (HomeFragment) fm.findFragmentByTag("Home");
+        TopRatedFragment top = (TopRatedFragment) fm.findFragmentByTag("Top");
+        
+        FragmentTransaction ft = fm.beginTransaction();
+
+        /** Detaches the androidfragment if exists */
+        if(home!=null)
+            ft.detach(home);
+
+        /** Detaches the applefragment if exists */
+        if(top!=null)
+            ft.detach(top);
+
+        /** If current tab is android */
+        if(tabId.equalsIgnoreCase("Home")){
+
+            if(home==null){
+                /** Create AndroidFragment and adding to fragmenttransaction */
+                ft.add(R.id.realtabcontent,new TopRatedFragment(), "Top");
+            }else{
+                /** Bring to the front, if already exists in the fragmenttransaction */
+                ft.attach(home);
             }
-        };
-        tHost.setOnTabChangedListener(tabChangeListener);
- 
-        TabHost.TabSpec topTab = tHost.newTabSpec("Top");
-        topTab.setIndicator(null,getResources().getDrawable(R.drawable.btn_toprated));
-        topTab.setContent(new TabContent(getBaseContext()));
-        tHost.addTab(topTab);
-        
-        TabHost.TabSpec searchTab = tHost.newTabSpec("Search");
-        searchTab.setIndicator(null,getResources().getDrawable(R.drawable.btn_search));
-        searchTab.setContent(new TabContent(getBaseContext()));
-        tHost.addTab(searchTab);
-        
-        TabHost.TabSpec tSpecAndroid = tHost.newTabSpec("Home");
-        tSpecAndroid.setIndicator(null,getResources().getDrawable(R.drawable.btn_home));
-        tSpecAndroid.setContent(new TabContent(getBaseContext()));
-        tHost.addTab(tSpecAndroid);
-        
-        TabHost.TabSpec settingsTab = tHost.newTabSpec("Settings");
-        settingsTab.setIndicator(null, getResources().getDrawable(R.drawable.btn_settings));
-        settingsTab.setContent(new TabContent(getBaseContext()));
-        tHost.addTab(settingsTab);
-        
-        TabHost.TabSpec favoritesTab = tHost.newTabSpec("Faves");
-        favoritesTab.setIndicator(null,getResources().getDrawable(R.drawable.btn_favorites));
+
+        }else{    /** If current tab is apple */
+            if(top==null){
+                /** Create AppleFragment and adding to fragmenttransaction */
+                ft.add(R.id.realtabcontent,new HomeFragment(), "Home");
+             }else{
+                /** Bring to the front, if already exists in the fragmenttransaction */
+                ft.attach(top);
+            }
+        }
+        ft.commit();
+    }
+    
+    private void addTab(String label, Drawable d) {
+    	TabHost.TabSpec favoritesTab = tabHost.newTabSpec(label);
+        favoritesTab.setIndicator(null, d);
         favoritesTab.setContent(new TabContent(getBaseContext()));
-        tHost.addTab(favoritesTab);
- 
+        tabHost.addTab(favoritesTab);
     }
 
 }
